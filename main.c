d91a3c6784efcdf33bf881b7a228cbf24b32ad40
Bill Yu <yutianzeabc@126.com>
2018-12-01 22:36:17 +0800

    UI

--- main.c
+++ main.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <ui.c>
 #include <list.c>
-//UI
+
 int main(int argc, char const *argv[])
 {
+    draw_main_ui();
     while (1){
-        //Draw UI
         char c;
-        printf("Please enter your option:");
         scanf("%c",&c);
-        //Options
         switch(c){
-            case 'x': return 0;
+            case 'x':
+            case 'X':
+                return 0;
+            case '1':
+                draw_input_ui();
+                break;
+            case '2':
+                draw_output_ui();
+                break;
+            case '3':
+                draw_query_ui();
+            case '4':
+                draw_modify_ui();
+            case '5':
+                draw_del_ui();
+                break;
+            default:
+                printf("\a");
+                draw_main_ui();
         }
     }
     return 0;
---
+++ ui.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+void draw_main_ui(){
+    system("cls");
+    system("title 图书信息管理系统");
+    system("color f0");
+    printf("*====================================*\n");
+    printf("#                                    #\n");
+    printf("#          图书信息管理系统          #\n");
+    printf("#                                    #\n");
+    printf("*====================================*\n");
+    printf("#                                    #\n");
+    printf("#              [1] 录 入             #\n");
+    printf("#                                    #\n");
+    printf("#              [2] 输 出             #\n");
+    printf("#                                    #\n");
+    printf("#              [3] 查 询             #\n");
+    printf("#                                    #\n");
+    printf("#              [4] 修 改             #\n");
+    printf("#                                    #\n");
+    printf("#              [5] 删 除             #\n");
+    printf("#                                    #\n");
+    printf("#              [X] 退 出             #\n");
+    printf("#                                    #\n");
+    printf("*====================================*\n");
+    printf("      Please enter your option:");
+    return;
+}
+void draw_input_ui(){
+    system("cls");
+    printf("Please input a new record with a line (input # to end):\n");
+    printf("e.g:登录号#书名#作者名#分类号#出版单位#出版时间#价格\n");
+    return;
+}
+void draw_output_ui(){
+    system("cls");
+    return;
+}
+void draw_query_ui(){
+    system("cls");
+    printf("*====================================*\n");
+    printf("#                                    #\n");
+    printf("#          图书信息管理系统          #\n");
+    printf("#                                    #\n");
+    printf("*====================================*\n");
+    printf("#                                    #\n");
+    printf("#              [1] 作者名            #\n");
+    printf("#                                    #\n");
+    printf("#              [2] 书目名            #\n");
+    printf("#                                    #\n");
+    printf("#              [X] 返 回             #\n");
+    printf("#                                    #\n");
+    printf("*====================================*\n");
+    return;
+}
+void draw_modify_ui(){
+    system("cls");
+    printf("Please input a book ID:\n");
+    return;
+} 
+void draw_del_ui(){
+    system("cls");
+    printf("Please input a book ID:\n");
+    return;
+} 
