b93794f043bc43a786d0488a20d387293834b8eb
Bill Yu <yutianzeabc@126.com>
2018-12-23 14:15:17 +0800

    File Bugs Comfirm

--- file.c
+++ file.c
@@ -5,13 +5,24 @@
     # include <stdbool.h>
 #endif
 
+
+#ifndef __WIN_H__
+    #define __WIN_H__  
+    #include <windows.h>
+    #include <winuser.h>
+    #include <conio.h>
+#endif
+
+extern int index;
+
+#include "type.h"
+
 //#define __DEBUG_FILE__
 
 #ifdef __DEBUG_FILE__
     #include "ui.c"
-    #include "list.c"
+    #include "opt.c"
 #endif
-extern int index;
 
 FILE *file_open_read();//打开文件为读取状态
 FILE *file_open_write();//打开文件为覆写状态
@@ -20,11 +31,11 @@ struct book *file_sync_read(FILE *stream);//读取文件
 bool file_sync_write(struct book *begin,FILE *stream);//写入文件
 
 FILE *file_open_read(){
-    return fopen("books.dat","r+b");
+    return fopen("books.dat","rb");
 }
 
 FILE *file_open_write(){
-    return fopen("books.dat","w+b");
+    return fopen("books.dat","wb");
 }
 
 bool file_close(FILE *stream){
@@ -34,12 +45,10 @@ bool file_close(FILE *stream){
 
 struct book *file_sync_read(FILE *stream){
     struct book *begin=form_new();
-    fread(&index,sizeof(int),1,stream);
+    fread(&index,sizeof(index),1,stream);
     while (true){
         struct book *buffer=(struct book *)malloc(sizeof(struct book));
-        if (fread(buffer,sizeof(struct book),1,stream)){
-            push_back(buffer,begin);
-        }
+        if (fread(buffer,sizeof(struct book),1,stream)) push_back(buffer,begin);
         else {
             free(buffer);
             break;
@@ -47,12 +56,12 @@ struct book *file_sync_read(FILE *stream){
     }
     return begin;
 }
-
+//FIXME:文件读写有误
 bool file_sync_write(struct book *begin,FILE *stream){
     struct book *buffer=begin;
-    fwrite(&index,sizeof(int),1,stream);
+    fwrite(&index,sizeof(index),1,stream);
     while (buffer!=NULL){
-        if (fwrite(buffer,sizeof(struct book),1,stream)==0) return false;
+        if (fwrite(buffer,sizeof(struct book),1,stream)!=1) return false; 
         buffer=buffer->back;
     }
     return true;
