f720addb9dab41ff2f60022ac7bde89330bd8e3c
Bill Yu <yutianzeabc@126.com>
2018-12-23 14:11:43 +0800

    Typedef

---
+++ type.h
@@ -0,0 +1,6 @@
+#pragma once
+struct book{
+    char regist[20],name[20],author[20],type[20],publish[20],publish_time[20],price[20];
+    struct book *forward,*back;//Á´±í
+    char* quality[7];
+};
